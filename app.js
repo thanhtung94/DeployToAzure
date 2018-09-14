a7842f6608d109ed3369d23583755515b3f2ebc9
thanhtung94 <nguyen.tung.2201@github.com>
2018-09-14 19:36:53 +0300

    task2-at-home

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
+var tungnguyenRouter = require('./routes/tungnguyen');
+
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
+app.use('/about', tungnguyenRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

186f4f6cf80e474fd44e163edc2319e2bd1e5205
thanhtung94 <nguyen.tung.2201@github.com>
2018-09-14 19:46:59 +0300

    task2-at-home-updated-content

--- routes/tungnguyen.js
+++ routes/tungnguyen.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    res.send('My deployment github repository can be found at https://github.com/thanhtung94/DeployToAzure');
 });
 
 module.exports = router;

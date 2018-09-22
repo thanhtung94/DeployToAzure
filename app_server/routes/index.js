2293c713842c4ca8257e2daca93921417b2c7306
thanhtung94 <nguyen.tung.2201@github.com>
2018-09-22 21:52:20 +0300

    task3-edited

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const main = require('../controllers/main');
+const ctrlMain = require('../controllers/ctrlMain');
 const ctrlFootball = require('../controllers/ctrlFootball');
 const ctrlGoldenBall = require('../controllers/ctrlGoldenBall');
 
-var ctrlMain = require('../controllers/main');
+
 /* GET home page. */
 router.get('/', ctrlMain.index());
 router.get('/football', ctrlFootball.winnerlist);
